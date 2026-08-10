8c8d14ceae29adadb3285dac019fb018cbe05efa
Jay Karnik <jkarnik@newrelic.com>
2026-08-11 01:50:25 +0530

    Add Stage A+B Wireless section: AP health + active client count

--- assets/js/site-main.js
+++ assets/js/site-main.js
@@ -435,3 +435,17 @@ function renderBgpFlapDetector(siteName) {
 }
 
 registerSiteRenderer(renderBgpFlapDetector);
+
+// --- WIRELESS SECTION (Stage A+B; Time-to-Connect appended in Task 17) ---
+
+function renderWirelessSection(siteName) {
+    const aps = statusCounts(DataLoader.getDevicesBySite(siteName, 'accessPoints'));
+    const activeClients = DataLoader.getClientCountByType('accessPoints', siteName);
+
+    ['stageAB', 'stageABC'].forEach(tab => {
+        setText(`wirelessApHealth-${tab}`, formatStatusCounts(aps));
+        setText(`wirelessActiveClients-${tab}`, `${activeClients} clients`);
+    });
+}
+
+registerSiteRenderer(renderWirelessSection);
